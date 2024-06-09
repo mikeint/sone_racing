3b1cd1d5be3d7ce3e549897f652ea5b99fbd74ad
Michael Sansone <only.sansone@gmail.com>
2024-06-09 14:45:46 -0400

    fixing auth credential types

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,52 +1,43 @@
-import NextAuth, { NextAuthOptions, User as NextAuthUser } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcryptjs";
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import bcrypt from 'bcryptjs';
 import User from "@/models/User";
 import connect from "@/utils/db";
 
-// Define the types for the credentials
-interface Credentials {
-    email: string;
-    password: string;
-}
-
 const authOptions: NextAuthOptions = {
-    providers: [
-        CredentialsProvider({
-            id: "credentials",
-            name: "Credentials",
-            credentials: {
-                email: { label: "Email", type: "text" },
-                password: { label: "Password", type: "password" },
-            },
-            async authorize(credentials: Credentials | undefined) {
-                await connect();
-                try {
-                    const user = await User.findOne({ email: credentials?.email });
-                    if (user) {
-                        const isPasswordCorrect = await bcrypt.compare(
-                            credentials!.password,
-                            user.password
-                        );
-                        if (isPasswordCorrect) {
-                            return user;
-                        }
-                    }
-                } catch (err: any) {
-                    throw new Error(err.message);
-                }
-                return null;
-            },
-        }),
-    ],
-    callbacks: {
-        async signIn({ user, account }) {
-            if (account?.provider === "credentials") {
-                return true;
+  providers: [
+    {
+      id: 'credentials',
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      type: 'credentials',
+      async authorize(credentials) {
+        await connect();
+        try {
+          const user = await User.findOne({ email: credentials?.email });
+          if (user) {
+            const isPasswordCorrect = await bcrypt.compare(credentials!.password, user.password);
+            if (isPasswordCorrect) {
+              return user;
             }
-            return false;
-        },
+          }
+        } catch (err: any) {
+          throw new Error(err.message);
+        }
+        return null;
+      },
     },
+  ],
+  callbacks: {
+    async signIn({ user, account }) {
+      if (account?.provider === 'credentials') {
+        return true;
+      }
+      return false;
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
